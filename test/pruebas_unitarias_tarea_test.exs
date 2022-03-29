e903d9cded9bc587fdd124c5e8fb254be41013cd
Rass22 <100615564+Rass22@users.noreply.github.com>
2022-03-28 18:21:21 -0600

    Update pruebas_unitarias_tarea_test.exs

--- test/pruebas_unitarias_tarea_test.exs
+++ test/pruebas_unitarias_tarea_test.exs
@@ -3,27 +3,27 @@ defmodule InvoiceValidatorTest do
 
   Calendar.put_time_zone_database(Tzdata.TimeZoneDatabase)
 
-  @tz_cdmx "Mexico/General"
+  @tz_cdmx "America/Mexico_City"
 
-  @pac_dt DateTime.from_naive!(~N[2022-03-24 10:00:00], @tz_cdmx)
+  @pac_dt DateTime.from_naive!(~N[2022-03-23 15:06:35], @tz_cdmx)
 
   data =  [
 
    {"72 hrs atras",    "America/Tijuana",  ~N[2022-03-20 13:06:31],  :error},
    {"72 hrs atras",    "America/Mazatlan",  ~N[2022-03-20 14:06:31],  :error},
-   {"72 hrs atras",    "America/General",     ~N[2022-03-20 15:06:31],  :error},
+   {"72 hrs atras",    "America/Mexico_City",     ~N[2022-03-20 15:06:31],  :error},
    {"72 hrs atras",    "America/Cancun",     ~N[2022-03-20 16:06:31],  :error},
-   {"72 hrs atras",    "America/Tijuana",  ~N[2022-03-20 13:06:35],  :ok},
+   {"72 hrs atras",    "America/Tijuana",  ~N[2022-03-20 14:06:35],  :ok},
    {"72 hrs atras",    "America/Mazatlan",  ~N[2022-03-20 14:06:35],  :ok},
-   {"72 hrs atras",    "America/General",     ~N[2022-03-20 15:06:35],  :ok},
+   {"72 hrs atras",    "America/Mexico_City",     ~N[2022-03-20 15:06:35],  :ok},
    {"72 hrs atras",    "America/Cancun",     ~N[2022-03-20 16:06:35],  :ok},
-   {"5 mns adelante",  "America/Tijuana",  ~N[2022-03-23 13:11:35],  :ok},
+   {"5 mns adelante",  "America/Tijuana",  ~N[2022-03-23 14:11:35],  :ok},
    {"5 mns adelante",  "America/Mazatlan",  ~N[2022-03-23 14:11:35],  :ok},
-   {"5 mns adelante",  "America/General",     ~N[2022-03-23 15:11:35],  :ok},
+   {"5 mns adelante",  "America/Mexico_City",     ~N[2022-03-23 15:11:35],  :ok},
    {"5 mns adelante",  "America/Cancun",     ~N[2022-03-23 16:11:35],  :ok},
-   {"5 mns adelante",  "America/Tijuana",  ~N[2022-03-23 13:11:36],  :error},
+   {"5 mns adelante",  "America/Tijuana",  ~N[2022-03-23 14:11:36],  :error},
    {"5 mns adelante",  "America/Mazatlan",  ~N[2022-03-23 14:11:36],  :error},
-   {"5 mns adelante",  "America/General",     ~N[2022-03-23 15:11:36],  :error},
+   {"5 mns adelante",  "America/Mexico_City",     ~N[2022-03-23 15:11:36],  :error},
    {"5 mns adelante",  "America/Cancun",     ~N[2022-03-23 16:11:36],  :error},
 
   ]
